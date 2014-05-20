1757816ff4a0cc9bcead4504e9edff69ac4a128b
xufei <xu.fei@outlook.com>
2014-05-20 17:51:05 +0800

    同一级控制器下多个分页控件并存

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,8 @@
 angular.module("sn.tests", []);
 
 angular.module("sn.tests").controller("TestPagerCtrl1", function($scope) {
-	$scope.totalItems = 177;
+	$scope.totalItems1 = 177;
+	$scope.totalItems2 = 33;
 	
 	$scope.$on("sn.controls.pager:pageIndexChange", function(event) {
 		event.stopPropagation();
