4a22cc7fecbaf3f2901826c2a552e0eec1801291
xufei <xu.fei@outlook.com>
2014-05-21 17:58:18 +0800

    哦

--- scripts/test.js
+++ scripts/test.js
@@ -3,9 +3,36 @@ angular.module("sn.tests", []);
 angular.module("sn.tests").controller("TestPagerCtrl1", function($scope) {
 	$scope.totalItems1 = 177;
 	$scope.totalItems2 = 33;
-	
+
 	$scope.$on("sn.controls.pager:pageIndexChange", function(event) {
 		event.stopPropagation();
-		console.log(event.targetScope.currentPage);	
+		console.log(event.targetScope.currentPage);
 	})
 });
+
+angular.module("sn.tests").controller("TestTreeCtrl", function($scope) {
+	$scope.areaData = [{
+		name : "Jiangsu",
+		code : "js",
+		children : [{
+			name : "Nanjing",
+			code : "nj"
+		}, {
+			name : "Suzhou",
+			code : "sz",
+			children : [{
+				name : "Wujiang",
+				code : "wj"
+			}, {
+				name : "Changshu",
+				code : "cs"
+			}]
+		}]
+	}, {
+		name : "Yunnan",
+		code : "yn"
+	}, {
+		name : "Fujian",
+		code : "fj"
+	}];
+}); 
