a9bfa16e8217f1ca20d03251f997c3234f678680
xufei <xu.fei@outlook.com>
2014-08-25 17:20:50 +0800

    修改了日期控件的默认取值

--- scripts/controls/calendar.js
+++ scripts/controls/calendar.js
@@ -1,158 +1,164 @@
 angular.module("sn.controls").directive("snCalendar", [function () {
-    return {
-        restrict: "E",
-        scope: {},
-        controller: function ($scope) {
-            $scope.viewMode = 0;
+	return {
+		restrict: "E",
+		scope: {},
+		controller: function ($scope) {
+			$scope.viewMode = 0;
 
-            $scope.years = [];
-            $scope.months = ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"];
+			$scope.years = [];
+			$scope.months = ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"];
 
-            $scope.calendar = [];
-            $scope.weekdays = ["日", "一", "二", "三", "四", "五", "六"];
+			$scope.calendar = [];
+			$scope.weekdays = ["日", "一", "二", "三", "四", "五", "六"];
 
-            var now = new Date();
-            $scope.currentYear = now.getFullYear();
-            $scope.currentMonth = now.getMonth();
-            $scope.currentDate = now.getDate();
+			var now = new Date();
+			$scope.currentYear = now.getFullYear();
+			$scope.currentMonth = now.getMonth();
+			$scope.currentDate = now.getDate();
 
-            $scope.$watch("currentYear", function (newYear) {
-                $scope.$emit("sn.controls.calendar:yearChanged", newYear);
+			$scope.$watch("currentYear", function (newYear, oldYear) {
+				if (newYear != oldYear) {
+					$scope.$emit("sn.controls.calendar:yearChanged", newYear);
+				}
 
-                generateYears(newYear);
-                generateCalendar(newYear, $scope.currentMonth);
-            });
+				generateYears(newYear);
+				generateCalendar(newYear, $scope.currentMonth);
+			});
 
-            $scope.$watch("currentMonth", function (newMonth) {
-                $scope.$emit("sn.controls.calendar:monthChanged", newMonth);
+			$scope.$watch("currentMonth", function (newMonth, oldMonth) {
+				if (newMonth != oldMonth) {
+					$scope.$emit("sn.controls.calendar:monthChanged", newMonth);
+				}
 
-                generateCalendar($scope.currentYear, newMonth);
-            });
+				generateCalendar($scope.currentYear, newMonth);
+			});
 
-            $scope.$watch("currentDate", function(newDate) {
-                $scope.$emit("sn.controls.calendar:dateChanged", newDate);
-            });
+			$scope.$watch("currentDate", function(newDate, oldDate) {
+				if (newDate != oldDate) {
+					$scope.$emit("sn.controls.calendar:dateChanged", newDate);
+				}
+			});
 
-            function generateCalendar(year, month) {
-                $scope.calendar = [
-                    []
-                ];
+			function generateCalendar(year, month) {
+				$scope.calendar = [
+					[]
+				];
 
-                var offset = new Date(new Date(year, month, 1)).getDay();
-                var lastDay = new Date(new Date(year, month + 1, 1) - 1);
+				var offset = new Date(new Date(year, month, 1)).getDay();
+				var lastDay = new Date(new Date(year, month + 1, 1) - 1);
 
-                for (var day = offset; day < lastDay.getDate() + offset; day++) {
-                    if (!$scope.calendar[Math.floor(day / 7)]) {
-                        $scope.calendar[Math.floor(day / 7)] = [];
-                    }
-                    $scope.calendar[Math.floor(day / 7)][day % 7] = day - offset + 1;
-                }
-            }
+				for (var day = offset; day < lastDay.getDate() + offset; day++) {
+					if (!$scope.calendar[Math.floor(day / 7)]) {
+						$scope.calendar[Math.floor(day / 7)] = [];
+					}
+					$scope.calendar[Math.floor(day / 7)][day % 7] = day - offset + 1;
+				}
+			}
 
-            function generateYears(newYear) {
-                $scope.years = [];
-                var startIndex = Math.floor(newYear / 10) * 10 + 1;
+			function generateYears(newYear) {
+				$scope.years = [];
+				var startIndex = Math.floor(newYear / 10) * 10 + 1;
 
-                for (var year = 0; year < 10; year++) {
-                    $scope.years.push(startIndex + year);
-                }
-            }
+				for (var year = 0; year < 10; year++) {
+					$scope.years.push(startIndex + year);
+				}
+			}
 
-            $scope.dateClass = function (date) {
-                if ($scope.currentDate == date) {
-                    return "active today";
-                }
-                else {
-                    return "day";
-                }
-            };
+			$scope.dateClass = function (date) {
+				if ($scope.currentDate == date) {
+					return "active today";
+				}
+				else {
+					return "day";
+				}
+			};
 
-            $scope.monthClass = function (month) {
-                if ($scope.currentMonth == month) {
-                    return "active month";
-                }
-                else {
-                    return "month";
-                }
-            };
+			$scope.monthClass = function (month) {
+				if ($scope.currentMonth == month) {
+					return "active month";
+				}
+				else {
+					return "month";
+				}
+			};
 
-            $scope.yearClass = function (year) {
-                if ($scope.currentYear == year) {
-                    return "active year";
-                }
-                else {
-                    return "year";
-                }
-            };
+			$scope.yearClass = function (year) {
+				if ($scope.currentYear == year) {
+					return "active year";
+				}
+				else {
+					return "year";
+				}
+			};
 
-            $scope.selectDate = function (date) {
-                if (date) {
-                    $scope.currentDate = date;
-                }
-            };
+			$scope.selectDate = function (date) {
+				if (date) {
+					$scope.currentDate = date;
+				}
+			};
 
-            $scope.selectMonth = function (month) {
-                $scope.currentMonth = month;
-                $scope.switchView(0);
-            };
+			$scope.selectMonth = function (month) {
+				$scope.currentMonth = month;
+				$scope.switchView(0);
+			};
 
-            $scope.selectYear = function (year) {
-                $scope.currentYear = year;
-                $scope.switchView(1);
-            };
+			$scope.selectYear = function (year) {
+				$scope.currentYear = year;
+				$scope.switchView(1);
+			};
 
-            $scope.currentMonthStr = function () {
-                return $scope.currentYear + "年 " + $scope.months[$scope.currentMonth];
-            };
+			$scope.currentMonthStr = function () {
+				return $scope.currentYear + "年 " + $scope.months[$scope.currentMonth];
+			};
 
-            $scope.currentAgeStr = function () {
-                var startIndex = Math.floor($scope.currentYear / 10) * 10 + 1;
-                return startIndex + " - " + (startIndex + 9);
-            };
+			$scope.currentAgeStr = function () {
+				var startIndex = Math.floor($scope.currentYear / 10) * 10 + 1;
+				return startIndex + " - " + (startIndex + 9);
+			};
 
-            $scope.previousMonth = function () {
-                $scope.currentMonth--;
-                resetDate();
-            };
+			$scope.previousMonth = function () {
+				$scope.currentMonth--;
+				resetDate();
+			};
 
-            $scope.nextMonth = function () {
-                $scope.currentMonth++;
-                resetDate();
-            };
+			$scope.nextMonth = function () {
+				$scope.currentMonth++;
+				resetDate();
+			};
 
-            $scope.previousYear = function () {
-                $scope.currentYear--;
-                resetDate();
-            };
+			$scope.previousYear = function () {
+				$scope.currentYear--;
+				resetDate();
+			};
 
-            $scope.nextYear = function () {
-                $scope.currentYear++;
-                resetDate();
-            };
+			$scope.nextYear = function () {
+				$scope.currentYear++;
+				resetDate();
+			};
 
-            $scope.previousAge = function () {
-                $scope.currentYear -= 10;
-            };
+			$scope.previousAge = function () {
+				$scope.currentYear -= 10;
+			};
 
-            $scope.nextAge = function () {
-                $scope.currentYear += 10;
-            };
+			$scope.nextAge = function () {
+				$scope.currentYear += 10;
+			};
 
-            function resetDate() {
-                var current = new Date($scope.currentYear, $scope.currentMonth, $scope.currentDate);
+			function resetDate() {
+				var current = new Date($scope.currentYear, $scope.currentMonth, $scope.currentDate);
 
-                $scope.currentYear = current.getFullYear();
-                $scope.currentMonth = current.getMonth();
-                $scope.currentDate = current.getDate();
-            }
+				$scope.currentYear = current.getFullYear();
+				$scope.currentMonth = current.getMonth();
+				$scope.currentDate = current.getDate();
+			}
 
-            $scope.switchView = function (view) {
-                //0：日期；1：月；2：年
-                $scope.viewMode = view;
-            };
-        },
-        link: function (scope, element, attrs) {
-        },
-        templateUrl: "templates/calendar/calendar.html"
-    }
+			$scope.switchView = function (view) {
+				//0：日期；1：月；2：年
+				$scope.viewMode = view;
+			};
+		},
+		link: function (scope, element, attrs) {
+		},
+		templateUrl: "templates/calendar/calendar.html"
+	}
 }]);
--- scripts/controls/timepicker.js
+++ scripts/controls/timepicker.js
@@ -1,117 +1,123 @@
 angular.module("sn.controls").directive("snTimepicker", [function () {
-    return {
-        restrict: "E",
-        scope: {},
-        controller: function ($scope) {
-            $scope.viewMode = 3;
+	return {
+		restrict: "E",
+		scope: {},
+		controller: function ($scope) {
+			$scope.viewMode = 3;
 
-            $scope.hours = [];
-            $scope.minutes = [];
-            $scope.seconds = [];
+			$scope.hours = [];
+			$scope.minutes = [];
+			$scope.seconds = [];
 
-            for (var i = 0; i < 24; i++) {
-                $scope.hours.push(i);
-            }
+			for (var i = 0; i < 24; i++) {
+				$scope.hours.push(i);
+			}
 
-            for (var j = 0; j < 60; j++) {
-                $scope.minutes.push(j);
-            }
+			for (var j = 0; j < 60; j++) {
+				$scope.minutes.push(j);
+			}
 
-            for (var k = 0; k < 60; k++) {
-                $scope.seconds.push(k);
-            }
+			for (var k = 0; k < 60; k++) {
+				$scope.seconds.push(k);
+			}
 
-            var now = new Date();
-            $scope.currentHour = now.getHours();
-            $scope.currentMinute = now.getMinutes();
-            $scope.currentSecond = now.getSeconds();
+			var now = new Date();
+			$scope.currentHour = now.getHours();
+			$scope.currentMinute = now.getMinutes();
+			$scope.currentSecond = now.getSeconds();
 
-            $scope.$watch("currentHour", function(newHour) {
-                $scope.$emit("sn.controls.timePicker:hourChanged", newHour);
-            });
+			$scope.$watch("currentHour", function(newHour, oldHour) {
+				if (newHour != oldHour) {
+					$scope.$emit("sn.controls.timePicker:hourChanged", newHour);
+				}
+			});
 
-            $scope.$watch("currentMinute", function(newMinute) {
-                $scope.$emit("sn.controls.timePicker:minuteChanged", newMinute);
-            });
+			$scope.$watch("currentMinute", function(newMinute, oldMinute) {
+				if (newMinute != oldMinute) {
+					$scope.$emit("sn.controls.timePicker:minuteChanged", newMinute);
+				}
+			});
 
 
-            $scope.$watch("currentSecond", function(newSecond) {
-                $scope.$emit("sn.controls.timePicker:secondChanged", newSecond);
-            });
+			$scope.$watch("currentSecond", function(newSecond, oldSecond) {
+				if (newSecond != oldSecond) {
+					$scope.$emit("sn.controls.timePicker:secondChanged", newSecond);
+				}
+			});
 
-            $scope.hourClass = function (hour) {
-                if ($scope.currentHour == hour) {
-                    return "active hour";
-                }
-                else {
-                    return "hour";
-                }
-            };
+			$scope.hourClass = function (hour) {
+				if ($scope.currentHour == hour) {
+					return "active hour";
+				}
+				else {
+					return "hour";
+				}
+			};
 
-            $scope.minuteClass = function (minute) {
-                if ($scope.currentMinute == minute) {
-                    return "active minute";
-                }
-                else {
-                    return "minute";
-                }
-            };
+			$scope.minuteClass = function (minute) {
+				if ($scope.currentMinute == minute) {
+					return "active minute";
+				}
+				else {
+					return "minute";
+				}
+			};
 
-            $scope.secondClass = function (second) {
-                if ($scope.currentSecond == second) {
-                    return "active second";
-                }
-                else {
-                    return "second";
-                }
-            };
+			$scope.secondClass = function (second) {
+				if ($scope.currentSecond == second) {
+					return "active second";
+				}
+				else {
+					return "second";
+				}
+			};
 
-            $scope.selectHour = function (hour) {
-                $scope.currentHour = hour;
-                $scope.switchView(3);
-            };
+			$scope.selectHour = function (hour) {
+				$scope.currentHour = hour;
+				$scope.switchView(3);
+			};
 
-            $scope.selectMinute = function (minute) {
-                $scope.currentMinute = minute;
-                $scope.switchView(3);
-            };
+			$scope.selectMinute = function (minute) {
+				$scope.currentMinute = minute;
+				$scope.switchView(3);
+			};
 
-            $scope.selectSecond = function (second) {
-                $scope.currentSecond = second;
-                $scope.switchView(3);
-            };
+			$scope.selectSecond = function (second) {
+				$scope.currentSecond = second;
+				$scope.switchView(3);
+			};
 
-            $scope.increaseHour = function () {
-                $scope.currentHour = ($scope.currentHour + 1) % 24;
-            };
+			$scope.increaseHour = function () {
+				$scope.currentHour = ($scope.currentHour + 1) % 24;
+			};
 
-            $scope.decreaseHour = function () {
-                $scope.currentHour = ($scope.currentHour + 23) % 24;
-            };
+			$scope.decreaseHour = function () {
+				$scope.currentHour = ($scope.currentHour + 23) % 24;
+			};
 
-            $scope.increaseMinute = function () {
-                $scope.currentMinute = ($scope.currentMinute + 1) % 60;
-            };
+			$scope.increaseMinute = function () {
+				$scope.currentMinute = ($scope.currentMinute + 1) % 60;
+			};
 
-            $scope.decreaseMinute = function () {
-                $scope.currentMinute = ($scope.currentMinute + 59) % 60;
-            };
+			$scope.decreaseMinute = function () {
+				$scope.currentMinute = ($scope.currentMinute + 59) % 60;
+			};
 
-            $scope.increaseSecond = function () {
-                $scope.currentSecond = ($scope.currentSecond + 1) % 60;
-            };
+			$scope.increaseSecond = function () {
+				$scope.currentSecond = ($scope.currentSecond + 1) % 60;
+			};
 
-            $scope.decreaseSecond = function () {
-                $scope.currentSecond = ($scope.currentSecond + 59) % 60;
-            };
+			$scope.decreaseSecond = function () {
+				$scope.currentSecond = ($scope.currentSecond + 59) % 60;
+			};
 
-            $scope.switchView = function (view) {
-                //0：整体视图；1：时；2：分；3：秒
-                $scope.viewMode = view;
-            };
-        },
-        link: function (scope, element, attrs) {
-        },
-        templateUrl: "templates/timepicker/timepicker.html"
-    }
+			$scope.switchView = function (view) {
+				//0：整体视图；1：时；2：分；3：秒
+				$scope.viewMode = view;
+			};
+		},
+		link: function (scope, element, attrs) {
+		},
+		templateUrl: "templates/timepicker/timepicker.html"
+	}
 }]);
