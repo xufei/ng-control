57de6264019de9ac70fd8f1a7d2887aafbd34483
xufei <xu.fei@outlook.com>
2014-09-02 17:04:04 +0800

    分页的动态调整序号

--- scripts/controls/pager.js
+++ scripts/controls/pager.js
@@ -1,142 +1,149 @@
 angular.module("sn.controls").directive('snPager', function () {
-    return {
-        restrict: 'E',
-        scope: {},
-        controller: function ($scope, pagerConfig) {
-            $scope.pages = [];
+	return {
+		restrict: 'E',
+		scope: {},
+		controller: function ($scope, pagerConfig) {
+			$scope.pages = [];
 
-            $scope.currentPage = 0;
-            $scope.totalPages = 1;
-            $scope.totalItems = 0;
-            $scope.pageOffset = 0;
+			$scope.currentPage = 0;
+			$scope.totalPages = 1;
+			$scope.totalItems = 0;
+			$scope.pageOffset = 0;
 
-            $scope.$watch("totalItems", function () {
-                $scope.reset();
+			var initialized = false;
 
-                if ($scope.pages[$scope.currentPage - $scope.pageOffset]) {
-                    $scope.pages[$scope.currentPage - $scope.pageOffset].active = true;
-                }
-            });
+			$scope.$watch("totalItems", function () {
+				if ($scope.totalItems % $scope.itemsPerPage == 0) {
+					$scope.totalPages = $scope.totalItems / $scope.itemsPerPage;
+				} else {
+					$scope.totalPages = Math.ceil($scope.totalItems / $scope.itemsPerPage);
+				}
 
-            $scope.reset = function () {
-                if ($scope.totalItems % $scope.itemsPerPage == 0) {
-                    $scope.totalPages = $scope.totalItems / $scope.itemsPerPage;
-                } else {
-                    $scope.totalPages = Math.ceil($scope.totalItems / $scope.itemsPerPage);
-                }
+				if ($scope.totalPages == 0) {
+					$scope.totalPages = 1;
+				}
 
-                if ($scope.totalPages == 0) {
-                    $scope.totalPages = 1;
-                }
+				if (initialized) {
+					if (($scope.pageOffset > $scope.totalPages)) {
+						$scope.pageOffset = 0;
 
-                resetPageList();
-            };
+						if (($scope.currentPage < $scope.pageOffset)
+							|| ($scope.currentPage >= $scope.pageOffset + $scope.pages.length)) {
+							$scope.currentPage = 0;
+						}
+					}
+				}
+				resetPageList();
 
-            function getOffset(page) {
-                var offset = Math.min(page, $scope.totalPages - $scope.listSize);
-                if (offset < 0) {
-                    offset = 0;
-                }
+				initialized = true;
 
-                return offset;
-            }
+				if ($scope.pages[$scope.currentPage - $scope.pageOffset]) {
+					$scope.pages[$scope.currentPage - $scope.pageOffset].active = true;
+				}
+			});
 
-            function tryResetPageList(page) {
-                var offset = getOffset(page);
-                if (offset != $scope.pageOffset) {
-                    $scope.pageOffset = offset;
-                    resetPageList();
-                }
-            }
+			function getOffset(page) {
+				var offset = Math.min(page, $scope.totalPages - $scope.listSize);
+				if (offset < 0) {
+					offset = 0;
+				}
 
-            function resetPageList() {
-                $scope.pages = [];
+				return offset;
+			}
 
-                var last = Math.min($scope.pageOffset + $scope.listSize, $scope.totalPages);
-                for (var i = $scope.pageOffset; i < last; i++) {
-                    $scope.pages.push({
-                        text: i,
-                        pageIndex: i,
-                        active: false
-                    });
-                }
-            }
+			function resetPageList() {
+				$scope.pages = [];
 
-            $scope.getText = function (key) {
-                return pagerConfig.text[key];
-            };
+				var last = Math.min($scope.pageOffset + $scope.listSize, $scope.totalPages);
+				for (var i = $scope.pageOffset; i < last; i++) {
+					$scope.pages.push({
+						text: i,
+						pageIndex: i,
+						active: false
+					});
+				}
+			};
 
-            $scope.isFirst = function () {
-                return $scope.currentPage <= 0;
-            };
+			$scope.getText = function (key) {
+				return pagerConfig.text[key];
+			};
 
-            $scope.isLast = function () {
-                return $scope.currentPage >= $scope.totalPages - 1;
-            };
+			$scope.isFirst = function () {
+				return $scope.currentPage <= 0;
+			};
 
-            $scope.selectPage = function (value) {
-                if ((value >= $scope.totalPages) || (value < 0)) {
-                    return;
-                }
+			$scope.isLast = function () {
+				return $scope.currentPage >= $scope.totalPages - 1;
+			};
 
-                if ($scope.pages[$scope.currentPage - $scope.pageOffset]) {
-                    $scope.pages[$scope.currentPage - $scope.pageOffset].active = false;
-                }
+			$scope.selectPage = function (value) {
+				if ((value >= $scope.totalPages) || (value < 0)) {
+					return;
+				}
 
-	            if ((value < $scope.pageOffset) || (value >= $scope.pageOffset + $scope.pages.length)) {
-		            tryResetPageList(value);
-	            }
-                $scope.currentPage = value;
+				if ($scope.pages[$scope.currentPage - $scope.pageOffset]) {
+					$scope.pages[$scope.currentPage - $scope.pageOffset].active = false;
+				}
 
-                $scope.pages[$scope.currentPage - $scope.pageOffset].active = true;
+				if ((value < $scope.pageOffset) || (value >= $scope.pageOffset + $scope.pages.length)) {
+					var offset = getOffset(value);
+					if (offset != $scope.pageOffset) {
+						$scope.pageOffset = offset;
+						resetPageList();
+					}
+				}
 
-                $scope.$emit("sn.controls.pager:pageIndexChanged", $scope.pages[$scope.currentPage - $scope.pageOffset]);
-            };
+				$scope.currentPage = value;
 
-            $scope.first = function () {
-                if (this.isFirst()) {
-                    return;
-                }
-                this.selectPage(0);
-            };
+				$scope.pages[$scope.currentPage - $scope.pageOffset].active = true;
 
-            $scope.last = function () {
-                if (this.isLast()) {
-                    return;
-                }
-                this.selectPage(this.totalPages - 1);
-            };
+				$scope.$emit("sn.controls.pager:pageIndexChanged", $scope.pages[$scope.currentPage - $scope.pageOffset]);
+			};
 
-            $scope.previous = function () {
-                if (this.isFirst()) {
-                    return;
-                }
-                this.selectPage(this.currentPage - 1);
-            };
+			$scope.first = function () {
+				if (this.isFirst()) {
+					return;
+				}
+				this.selectPage(0);
+			};
 
-            $scope.next = function () {
-                if (this.isLast()) {
-                    return;
-                }
-                this.selectPage(this.currentPage + 1);
-            };
-        },
-        link: function (scope, element, attrs) {
-            scope.itemsPerPage = (attrs["itemsperpage"] - 0) || 10;
-            scope.listSize = (attrs["listsize"] - 0) || 10;
+			$scope.last = function () {
+				if (this.isLast()) {
+					return;
+				}
+				this.selectPage(this.totalPages - 1);
+			};
 
-            attrs.$observe("totalitems", function (value) {
-                scope.totalItems = value;
-            });
-        },
-        templateUrl: 'templates/pager/pager.html'
-    };
+			$scope.previous = function () {
+				if (this.isFirst()) {
+					return;
+				}
+				this.selectPage(this.currentPage - 1);
+			};
+
+			$scope.next = function () {
+				if (this.isLast()) {
+					return;
+				}
+				this.selectPage(this.currentPage + 1);
+			};
+		},
+		link: function (scope, element, attrs) {
+			scope.itemsPerPage = (attrs.itemsperpage - 0) || 10;
+			scope.listSize = (attrs.listsize - 0) || 10;
+
+			attrs.$observe("totalitems", function (value) {
+				scope.totalItems = value;
+			});
+		},
+		templateUrl: 'templates/pager/pager.html'
+	};
 }).constant('pagerConfig', {
-    itemsPerPage: 10,
-    text: {
-        first: '首页',
-        previous: '上一页',
-        next: '下一页',
-        last: '末页'
-    }
+	itemsPerPage: 10,
+	text: {
+		first: '首页',
+		previous: '上一页',
+		next: '下一页',
+		last: '末页'
+	}
 });
