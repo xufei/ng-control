2cca8e5fba45cfab6ed29e11a4991de62202085b
xufei <xu.fei@outlook.com>
2014-11-25 16:18:40 +0800

    右键菜单还是有bug

--- scripts/controls/contextmenu.js
+++ scripts/controls/contextmenu.js
@@ -1,48 +1,45 @@
 angular.module("sn.controls").directive("snContextmenu", ["$document", "$http", "$compile", "$rootScope", function ($document, $http, $compile, $rootScope) {
 	var currentMenu;
 
-    return {
-        restrict: "A",
-		scope: {
-			menuArr: "=snContextmenu"
-		},
-        link: function (scope, element, attrs) {
-	        $http.get("templates/menu/menu.html").then(function(result) {
-		        var menu = angular.element(result.data);
+	return {
+		restrict: "A",
+		link: function (scope, element, attrs) {
+			$http.get("templates/menu/menu.html").then(function(result) {
+				var menu = angular.element(result.data);
 
-		        $compile(menu)(angular.extend($rootScope.$new(), {
-			        menuArr: scope.menuArr
-		        }));
+				$compile(menu)(angular.extend($rootScope.$new(), {
+					menuArr: scope.$eval(attrs["snContextmenu"])
+				}));
 
-		        element.on("contextmenu", function (evt) {
-			        var mouseX = evt.clientX;
-			        var mouseY = evt.clientY;
+				element.on("contextmenu", function (evt) {
+					var mouseX = evt.clientX;
+					var mouseY = evt.clientY;
 
-			        if ($document.find("body")[0].contains(menu[0])) {
-				        menu.css("display", "block");
-			        }
-			        else {
-				        $document.find("body").append(menu);
-			        }
+					if ($document.find("body")[0].contains(menu[0])) {
+						menu.css("display", "block");
+					}
+					else {
+						$document.find("body").append(menu);
+					}
 
-			        menu.css("display", "block");
-			        menu.css("left", mouseX + "px");
-			        menu.css("top", mouseY + "px");
+					menu.css("display", "block");
+					menu.css("left", mouseX + "px");
+					menu.css("top", mouseY + "px");
 
-			        evt.stopPropagation();
-			        evt.preventDefault();
+					evt.stopPropagation();
+					evt.preventDefault();
 
-			        if (currentMenu && currentMenu != menu) {
-				        currentMenu.css("display", "none");
-			        }
+					if (currentMenu && currentMenu != menu) {
+						currentMenu.css("display", "none");
+					}
 
-			        currentMenu = menu;
-		        });
+					currentMenu = menu;
+				});
 
-		        $document.on("click", function (evt) {
-			        menu.css("display", "none");
-		        });
-	        });
-        }
-    };
+				$document.on("click", function (evt) {
+					menu.css("display", "none");
+				});
+			});
+		}
+	};
 }]);
