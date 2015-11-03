eef69b45c37a2eeb8dc09e278d29ce3d4606112b
xufei <xu.fei@outlook.com>
2015-11-03 09:38:24 +0800

    tooltip

--- src/components/behavior/tooltip/directives/tooltip.js
+++ src/components/behavior/tooltip/directives/tooltip.js
@@ -10,30 +10,55 @@ export default class ToolTipDirective {
         this.UIHelper = UIHelper;
         
         this.scope = {
-			content: "=snTooltip"
+			content: "=snTooltip",
+			direction: "="
 		};
     }
 
     link(scope, element, attrs) {
+		scope.direction = scope.direction || "right";
+		
         let tooltip = angular.element(template);
 
         this.$compile(tooltip)(scope);
 	
-		element.on("mouseenter", function (evt) {
+		element.on("mouseenter", evt => {
 			let target = evt.target;
 			let offset = this.UIHelper.getOffset(target);
 	
 			this.$document.find("body").append(tooltip);
 			tooltip.addClass("in");
 	
-			let x = offset.x + element[0].offsetWidth;
-			let y = offset.y + (element[0].offsetHeight - tooltip[0].offsetHeight) / 2;
+			let x;
+			let y;
+			switch (scope.direction) {
+				case "left": {
+					x = offset.x - tooltip[0].offsetWidth;
+					y = offset.y + (element[0].offsetHeight - tooltip[0].offsetHeight) / 2;
+					break;
+				}
+				case "right": {
+					x = offset.x + element[0].offsetWidth;
+					y = offset.y + (element[0].offsetHeight - tooltip[0].offsetHeight) / 2;
+					break;
+				}
+				case "top": {
+					x = offset.x + (element[0].offsetWidth - tooltip[0].offsetWidth) / 2;
+					y = offset.y - tooltip[0].offsetHeight;
+					break;
+				}
+				case "bottom": {
+					x = offset.x + (element[0].offsetWidth - tooltip[0].offsetWidth) / 2;
+					y = offset.y + element[0].offsetHeight;
+					break;
+				}
+			}
 	
 			tooltip.css("z-index", "1500");
 			tooltip.css("display", "block");
 			tooltip.css("left", x + "px");
 			tooltip.css("top", y + "px");
-		}.bind(this));
+		});
 	
 		element.on("mouseleave", function () {
 			tooltip.remove();
