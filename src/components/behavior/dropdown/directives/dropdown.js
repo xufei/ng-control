d94b31ad7c7cc969673d6b00e9c42f7070ee9c87
xufei <xu.fei@outlook.com>
2015-11-24 09:57:49 +0800

    dropdown

--- src/components/behavior/dropdown/directives/dropdown.js
+++ src/components/behavior/dropdown/directives/dropdown.js
@@ -3,14 +3,20 @@ import { UIHelper } from "../../../utils/ui-helper";
 export default class DropdownDirective {
     constructor() {
         this.restrict = "A";
+        
+        this.dropdowns = new Map();
     }
 
     link(scope, element) {
         angular.element(element.children()[0]).on("click", evt => {
             element.toggleClass("open");
 
-            //evt.preventDefault();
-            //evt.stopPropagation();
+            evt.preventDefault();
+            evt.stopPropagation();
+            
+            this.dropdowns.delete(this);
+            this.dropdowns.forEach(it => it.removeClass("open"));
+            this.dropdowns.set(this, element);
         });
 
         let closeEvent = UIHelper.listen(window, 'click', (e) => {
@@ -19,6 +25,10 @@ export default class DropdownDirective {
             //}
         });
 		
-		scope.$on('$destroy', () => closeEvent.remove());
+		scope.$on('$destroy', () => {
+            closeEvent.remove();
+            
+            this.dropdowns.delete(this);
+        });
     }
 }
