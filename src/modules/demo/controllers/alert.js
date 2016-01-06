8537cb136a18aa1a20d97ee3f545526e997824e1
xufei <xu.fei@outlook.com>
2016-01-06 19:48:09 +0800

    alert 加了多行文本

--- src/modules/demo/controllers/alert.js
+++ src/modules/demo/controllers/alert.js
@@ -34,7 +34,7 @@ export default class AlertController {
 		this.AlertService
 			.confirm({
 				title: "测试",
-				content: "你好，确定吗？"
+				contents: ["你好，确定要删除以下内容吗？", "aaa, bbb"]
 			})
 			.then(function () {
 				alert("你刚才点了确定");
