d220fe1f39da576bc3769f97fa9817f55a2603eb
xufei <xu.fei@outlook.com>
2016-01-19 15:05:29 +0800

    datetimepicker default date

--- src/components/form/datetime-picker/controllers/datetimepicker.js
+++ src/components/form/datetime-picker/controllers/datetimepicker.js
@@ -1,20 +1,21 @@
 import { Calendar } from "../../../models/calendar/calendar";
 
 export default class DateTimePickerController extends Calendar {
-	constructor($filter, $timeout) {
-		super();
-		
-		this.$filter = $filter;
-		this.$timeout = $timeout;
-		
-		if ((this.selectedDate == null) || isNaN(this.selectedDate.getTime())) {
-			this.currentDate = new Date();
-		}
-		else {
-			this.currentDate = this.selectedDate;
-		}
-	}
-    
+    constructor($filter, $timeout) {
+        super();
+
+        this.$filter = $filter;
+        this.$timeout = $timeout;
+
+        if ((this.selectedDate == null) || isNaN(this.selectedDate.getTime())) {
+            this.currentDate = new Date();
+        }
+        else {
+            this.currentDate = this.selectedDate;
+            this.updateStr();
+        }
+    }
+
     get currentDate() {
         if (this.showTime) {
             return new Date(this.year, this.month, this.date, this.hour, this.minute, this.second);
@@ -23,44 +24,43 @@ export default class DateTimePickerController extends Calendar {
             return new Date(this.year, this.month, this.date);
         }
     }
-    
+
     set currentDate(val) {
-		if (val) {
-			this.year = val.getFullYear();
-			this.month = val.getMonth();
-			this.date = val.getDate();
-            
+        if (val) {
+            this.year = val.getFullYear();
+            this.month = val.getMonth();
+            this.date = val.getDate();
+
             if (this.showTime) {
                 this.hour = val.getHours();
                 this.minute = val.getMinutes();
                 this.second = val.getSeconds();
             }
-		}
-		this.updateStr();
+        }
     }
-	
-	updateStr() {
-		if (this.showTime) {
-			this.currentDateStr = this.$filter("date")(this.currentDate, "yyyy-MM-dd hh:mm:ss");
-		}
-		else {
-			this.currentDateStr = this.$filter("date")(this.currentDate, "yyyy-MM-dd");
-		}
-	}
 
-	showPop() {
-		if (!this.disabled) {
-			this.pop = true;
-		}
-	}
+    updateStr() {
+        if (this.showTime) {
+            this.currentDateStr = this.$filter("date")(this.currentDate, "yyyy-MM-dd hh:mm:ss");
+        }
+        else {
+            this.currentDateStr = this.$filter("date")(this.currentDate, "yyyy-MM-dd");
+        }
+    }
 
-	dateClick() {
-		this.$timeout(() => {
-			this.selectedDate = this.currentDate;
-			this.updateStr();
-			this.pop = false;
-		}, 0);
-	};
+    showPop() {
+        if (!this.disabled) {
+            this.pop = true;
+        }
+    }
+
+    dateClick() {
+        this.$timeout(() => {
+            this.selectedDate = this.currentDate;
+            this.updateStr();
+            this.pop = false;
+        }, 0);
+    };
 }
 
 DateTimePickerController.$inject = ["$filter", "$timeout"];
